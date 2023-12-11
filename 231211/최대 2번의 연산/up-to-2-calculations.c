7ea80d1e44a9d51a68c8a51a72731ee43f5ba7c7
Kimhyeonkyeong <145317652+Kimhyeonkyeong@users.noreply.github.com>
2023-12-11 18:48:58 +0900

    Title: 최대 2번의 연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231211/최대 2번의 연산/up-to-2-calculations.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        a = a/2;
+        if(a%2==1)
+        {
+            a = (a+1)/2;
+            printf("%d",a);
+        }
+        else
+        {
+            printf("%d",a);
+        }
+    }
+    return 0;
+}
